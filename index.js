48a1ccefd40c30b46e091ad0b6852688dabacb41
mkXultra <electrincgoat@gmail.com>
2023-03-16 19:15:21 +0900

    update

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ let running = false;
 const server = http.createServer(async (req, res) => {
   console.log(req.url);
   console.log(req.method);
-  console.log(req.method);
   if (req.method === 'POST') {
     const body = await getJsonBody(req)
     body.ref
